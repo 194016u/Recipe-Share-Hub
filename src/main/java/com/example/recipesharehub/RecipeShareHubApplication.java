6110878efe4f449c5908e36f7eecb9f1bf56f5a6
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-03 22:56:53 +0530

    Initial commit

---
+++ src/main/java/com/example/recipesharehub/RecipeShareHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipesharehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeShareHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeShareHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipesharehub/RecipeShareHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipesharehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeShareHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
