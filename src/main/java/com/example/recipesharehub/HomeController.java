1e6319580d6d71f7cc02bd79640f652ea7c07ebf
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-03 23:57:47 +0530

    create home controller

--- src/main/java/com/example/recipesharehub/HomeController.java
+++ src/main/java/com/example/recipesharehub/HomeController.java
@@ -1,4 +1,10 @@
 package com.example.recipesharehub;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    public String homeController(){
+        return "well";
+    }
 }
