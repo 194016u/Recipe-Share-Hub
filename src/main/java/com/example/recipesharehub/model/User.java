616291f742c2f845fb5cb682dd011ccb7d3e813e
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-04 13:35:04 +0530

    entity add

--- src/main/java/com/example/recipesharehub/model/User.java
+++ src/main/java/com/example/recipesharehub/model/User.java
@@ -1,4 +1,15 @@
 package com.example.recipesharehub.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter //we use lombok so no need create getters and setters
 public class User {
+    private Long id;
+    private String password;
+    private String email;
+    private String fullName;
+
+
 }
