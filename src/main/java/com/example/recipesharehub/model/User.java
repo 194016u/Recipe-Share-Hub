4105b195f32d3afe571fa47e83498d79bf5c5999
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-04 14:04:20 +0530

    create package repository

--- src/main/java/com/example/recipesharehub/model/User.java
+++ src/main/java/com/example/recipesharehub/model/User.java
@@ -1,11 +1,18 @@
 package com.example.recipesharehub.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter //we use lombok so no need create getters and setters
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String password;
     private String email;
