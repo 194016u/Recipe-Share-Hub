d8c2a871f741a17fb34c5526ef116b0e77ec759f
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-04 13:28:06 +0530

    get request test

--- src/main/java/com/example/recipesharehub/HomeController.java
+++ src/main/java/com/example/recipesharehub/HomeController.java
@@ -1,10 +1,12 @@
 package com.example.recipesharehub;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @GetMapping
     public String homeController(){
-        return "well";
+        return "welcome ";
     }
 }
